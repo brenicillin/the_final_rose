9f9678f8439a16b53cb1be9c941ce514338597a7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-23 12:57:47 -0400

    create model tests and outing show spec

--- spec/models/outing_spec.rb
+++ spec/models/outing_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Outing, type: :model do
     it {should have_many :contestant_outings}
     it {should have_many(:contestants).through(:contestant_outings)}
   end
+  
+  before(:all) do
+    test_data
+  end
+
+  describe 'instance methods' do
+    it '#find_contestants' do
+      expect(@outing_1.find_contestants).to eq(["John Paul Jones", "Tyler Cameron"])
+      expect(@outing_2.find_contestants).to eq(["Joe Coleman", "Nate Coleman"])
+      expect(@outing_3.find_contestants).to eq(["Brandon Jones", "Clayton Echard"])
+    end
+
+    it '#number_of_contestants' do
+      expect(@outing_1.number_of_contestants).to eq(2)
+      expect(@outing_2.number_of_contestants).to eq(2)
+      expect(@outing_3.number_of_contestants).to eq(2)
+    end
+  end
 end
