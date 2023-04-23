81b88aee7f75434bd2ab10338b1fa6a973b276fc
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-23 12:57:58 -0400

    create instance methods on outing model

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -1,4 +1,12 @@
 class Outing < ApplicationRecord
   has_many :contestant_outings
   has_many :contestants, through: :contestant_outings
+
+  def find_contestants
+    contestants.pluck(:name)
+  end
+
+  def number_of_contestants
+    contestants.count
+  end
 end
