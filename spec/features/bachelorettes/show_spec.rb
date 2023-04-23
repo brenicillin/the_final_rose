f0f3badcb16643d3f84bafe2237863542d53c401
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-23 11:45:52 -0400

    add bachelorette show and contestant index tests

--- spec/features/bachelorettes/show_spec.rb
+++ spec/features/bachelorettes/show_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Bachelorette Show Page, type: :feature do
+RSpec.describe "Bachelorette Show Page", type: :feature do
   before(:all) do
-    @bachelorette = Bachelorette.create!(name: "Hannah Brown", season_number: 15, description: "The most dramatic season yet!")
+    @bachelorette = Bachelorette.create!(name: "Hannah Brown", season_number: 15)
+    @bachelorette_2 = Bachelorette.create!(name: "Katie Thurston", season_number: 17)
   end
+
   describe 'User Story 1' do
     it 'can see bachelorette name, season number, and description' do
       visit "/bachelorettes/#{@bachelorette.id}"
@@ -14,18 +16,20 @@ RSpec.describe Bachelorette Show Page, type: :feature do
 
       within "#bachelorette-info" do
         expect(page).to have_content(@bachelorette.season_number)
-        expect(page).to have_content(@bachelorette.description)
       end
     end
 
     it 'shows a link to see the bachelorettes contestants' do
-      visit "/bachelorettes/#{@bachelorette.id}"
+      @contestant = @bachelorette.contestants.create!(name: "John Paul Jones", age: 25, hometown: "New York City")
+      @contestant_2 = @bachelorette_2.contestants.create!(name: "Tyler Cameron", age: 27, hometown: "Jupiter, Florida")
 
-      within"#bachelor-link" do
-        expect(page).to have_link("Bachelors")
-        click_link("Bachelors")
-      end
+      visit "/bachelorettes/#{@bachelorette.id}"
       
+      within"#bachelor-link" do
+        expect(page).to have_link("Meet the Bachelors!")
+        click_link("Meet the Bachelors!")
+      end
+
       expect(current_path).to eq("/bachelorettes/#{@bachelorette.id}/contestants")
     end
   end
